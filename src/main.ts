ad85eb834f57de73e24565544d6fa9bd797a0af1
draganAndonovski <draganandonovski995@gmail.com>
2016-08-10 14:18:28 +0200

    Router setup

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
+import { provideRouter } from '@angular/router';
 import { AppComponent, environment } from './app/';
+import { AppRoutes } from './app/app.routes';
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(AppComponent);
+bootstrap(AppComponent, [provideRouter(AppRoutes)]);
